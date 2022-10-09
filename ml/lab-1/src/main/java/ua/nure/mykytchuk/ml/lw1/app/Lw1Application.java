673e55662c32ef5e32c3e2f4dc95553e39d49f00
Mykytchuk Vladislav <vladislav.mykytchuk@extern.a1.at>
2022-10-09 12:45:42 +0300

    created ml.lab-1 project

---
+++ ml/lab-1/src/main/java/ua/nure/mykytchuk/ml/lw1/app/Lw1Application.java
@@ -0,0 +1,12 @@
+package ua.nure.mykytchuk.ml.lw1.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lw1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lw1Application.class, args);
+    }
+}
